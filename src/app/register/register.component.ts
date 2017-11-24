637b76bcf455ebe894ebbab4f0b3519e19b4c4a4
Patrick Reinke <reinkepatrick@yahoo.de>
2017-11-24 23:25:32 +0100

    added register + validation

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -6,10 +7,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
+  registerForm: FormGroup;
+  passwords: FormGroup;
 
   constructor() { }
 
   ngOnInit() {
+    this.passwords = new FormGroup({
+      'password': new FormControl('', [Validators.required, Validators.minLength(8)]),
+      'repeat': new FormControl('', [Validators.required])
+    }, {validators: RegisterComponent.isPasswordMatching});
+
+    this.registerForm = new FormGroup({
+      'username': new FormControl(null, [Validators.required, Validators.minLength(3), Validators.maxLength(16)]),
+      'email': new FormControl(null, [Validators.required, Validators.email]),
+      'passwords': this.passwords
+    });
   }
 
+  onSubmit() {
+    console.log(this.registerForm);
+    this.registerForm.reset();
+  }
+
+  static isPasswordMatching(control: FormGroup): {[s: string]: boolean} {
+    if(control == null) return null;
+    return control.get('repeat').value == control.get('password').value ? null : {'notMatching': true};
+  }
 }
