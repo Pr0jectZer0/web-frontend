7c60a8c516943734d87378e0860be3f63f667462
reinkepatrick <patrick.reinke@fh-bielefeld.de>
2017-11-30 20:00:36 +0100

    new structure

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from "@angular/core";
 import { tokenNotExpired } from "angular2-jwt";
-import {Token} from "../shared/token.model";
+import { Token } from "./token.model";
 
 @Injectable()
 export class AuthService {
