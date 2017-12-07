8be0aa5ea0a14d3e7a6ea16e4efde20e1bd2589e
RotblondesWeizen <turon.pascal@gmail.com>
2017-12-07 12:26:22 +0100

    added auth service and get request

--- src/app/game-library/game-library.component.ts
+++ src/app/game-library/game-library.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {AuthService} from '../auth/auth.service';
+import {HttpClient} from '@angular/common/http';
 
 @Component({
   selector: 'app-game-library',
@@ -7,9 +9,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GameLibraryComponent implements OnInit {
 
-  constructor() { }
+  constructor(private auth: AuthService, private http: HttpClient) {}
 
   ngOnInit() {
-  }
+    this.http.get('https://pr0jectzer0.ml/api/user/game/list?token=' + this.auth.getToken()).subscribe( data =>{
 
+    }, error =>{
+
+    );
+  }
 }
+
