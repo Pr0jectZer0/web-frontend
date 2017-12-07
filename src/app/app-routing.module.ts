61f35aade24121e0614c96b5393fe6a306251d52
RotblondesWeizen <turon.pascal@gmail.com>
2017-12-07 11:38:06 +0100

    added game-library

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,12 +5,14 @@ import { RouterModule, Routes } from '@angular/router';
 import { SignupComponent } from './auth/signup/signup.component';
 import { ErrorPageComponent } from './error-page/error-page.component';
 import { SigninComponent } from './auth/signin/signin.component';
+import {GameLibraryComponent} from './game-library/game-library.component';
 
 const appRoutes: Routes = [
   { path: '', redirectTo: '/signup', pathMatch: 'full' },
   { path: 'signup', component: SignupComponent },
   { path: 'signin', component: SigninComponent },
   { path: 'not-found', component: ErrorPageComponent, },
+  { path: 'game-library', component: GameLibraryComponent, },
   { path: '**', redirectTo: '/not-found'}
 ];
 
