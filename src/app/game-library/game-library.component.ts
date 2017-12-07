5dbfe3b67a2b94d5ddd531ced5b47e388f93ccb5
RotblondesWeizen <turon.pascal@gmail.com>
2017-12-07 22:16:25 +0100

    added discription, genre, publisher, release date and latest update

--- src/app/game-library/game-library.component.ts
+++ src/app/game-library/game-library.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import {AuthService} from '../auth/auth.service';
 import {HttpClient} from '@angular/common/http';
 import {Game} from "../shared/game.module";
+import {Publisher} from "../shared/publisher.model";
+import {Genre} from "../shared/genre.module";
 
 @Component({
   selector: 'app-game-library',
@@ -11,12 +13,20 @@ import {Game} from "../shared/game.module";
 export class GameLibraryComponent implements OnInit {
 
   games: Game[];
+  genres: Genre[];
+  publishers: Publisher[];
 
   constructor(private auth: AuthService, private http: HttpClient) {}
 
   ngOnInit() {
     this.http.get('https://pr0jectzer0.ml/api/user/game/list?token=' + this.auth.getToken()).subscribe( data => {
       this.games = data['games'];
+    });
+    this.http.get('https://pr0jectzer0.ml/api/genre').subscribe( data => {
+      this.genres = data['genres'];
+    });
+    this.http.get('https://pr0jectzer0.ml/api/publisher').subscribe( data => {
+      this.publishers = data['publishers'];
     });
   }
 }
