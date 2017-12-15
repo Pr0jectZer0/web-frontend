8df63e1044297c034b596534c3975ae2fd820f83
Pascal Turon <pturon@fh-bielefeld.de>
2017-12-15 16:38:34 +0100

    added responsive navigation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -20,7 +20,7 @@ import {GameDetailComponent} from "./game-library/game-detail/game-detail.compon
 import {LibraryService} from "./game-library/library.service";
 import {UsersService} from './shared/users.service';
 import { GroupProfileComponent } from './groups/group-profile/group-profile.component';
-import { GroupMemberlistComponent } from './group-memberlist/group-memberlist.component';
+import { GroupMemberlistComponent } from './groups/group-memberlist/group-memberlist.component';
 
 
 @NgModule({
