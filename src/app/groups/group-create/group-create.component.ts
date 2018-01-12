6ed57a29502ed7e891d7fc0ffcb9b4d241776145
Pascal Turon <pturon@fh-bielefeld.de>
2018-01-12 09:35:14 +0100

    interface for group creation

--- src/app/groups/group-create/group-create.component.ts
+++ src/app/groups/group-create/group-create.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormControl, FormGroup, NgForm, Validators} from "@angular/forms";
+import {isEmailValid} from "../../shared/is-email-valid.directive";
 
 @Component({
   selector: 'app-group-create',
@@ -7,9 +9,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GroupCreateComponent implements OnInit {
 
-  constructor() { }
+  createGroupForm: FormGroup;
+
+  constructor() {
+  }
 
   ngOnInit() {
+    this.createGroupForm = new FormGroup({
+      'name': new FormControl(null, [Validators.required]),
+      'desc': new FormControl(null, [Validators.required])
+    });
+  }
+
+  onCreateGroup(form: NgForm) {
+
   }
 
 }
