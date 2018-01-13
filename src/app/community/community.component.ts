703dff83b5c066d50d64898f5d0c0d853db2b60d
Pascal Turon <pturon@fh-bielefeld.de>
2018-01-13 14:50:48 +0100

    finished community component

--- src/app/community/community.component.ts
+++ src/app/community/community.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {GroupsService} from "../shared/groups.service";
 import {Group} from "../shared/group.model";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-community',
@@ -11,12 +12,17 @@ export class CommunityComponent implements OnInit {
 
   groups: Group[];
 
-  constructor(private groupService: GroupsService) { }
+  constructor(private groupService: GroupsService, private router: Router) {
+  }
 
   ngOnInit() {
     this.groupService.getAllGroups().subscribe(data => {
       this.groups = data['groups'];
     });
+  }
+
+  public groupClicked(id: number) {
+    this.router.navigate(['/group-profile', id]);
   }
 
 }
